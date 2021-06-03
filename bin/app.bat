41857e04bfa7ced28ae9e754fbbc0e2fee03c3da
master <master@devnull.local>
2021-06-03 19:42:46 +0300

    fix #2 - autoinstall dependencies offline

--- bin/app.bat
+++ bin/app.bat
@@ -3,15 +3,18 @@ rem :Setting up venv + install dependencies if this is first run
 rem :Starting app from venv if already set
 rem :Adjust APP_HOME to actual path of project
 
-SET APP_HOME=
-cd %APP_HOME%
-IF EXIST venv echo "Found venv"
-    .\venv\Scripts\activate && pythonw app.py 
-ELSE echo "Venv not found. Creating..."    
-    py -m venv venv &&
-    echo "Install dependencies" &&
-    .\venv\Scripts\activate &&
-    cd dependencies &&
-    .\venv\Scripts\pip3.9.exe install * -f ./ --no-index &&
+SET APP_HOME=C:\FTStats_v2.0.0\
+cd %APP_HOME%	
+if exist venv\ (
+	echo "Found venv"
+	.\venv\Scripts\activate && pythonw app.py 
+) else (
+	echo "Venv not found. Creating + activating"
+	py -m venv venv
+	venv\Scripts\activate
+    echo "Install dependencies"    
+	FOR %%i in (dep\*.whl) DO venv\Scripts\pip3.9.exe install %%i
+	FOR %%i in (dep\*.tar.gz) DO venv\Scripts\pip3.9.exe install %%i	
     cd %APP_HOME%
     pythonw app.py 
+)
